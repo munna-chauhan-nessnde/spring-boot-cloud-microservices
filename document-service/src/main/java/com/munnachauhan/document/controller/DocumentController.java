a6b572c724dafe0b17ce80b3fdf986ea75743aad
Munna Chauhan <munna.chauhan@ness.com>
2020-07-20 22:38:06 +0530

    removed RefreshScope

--- document-service/src/main/java/com/munnachauhan/document/controller/DocumentController.java
+++ document-service/src/main/java/com/munnachauhan/document/controller/DocumentController.java
@@ -1,7 +1,6 @@
 package com.munnachauhan.document.controller;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Munna Chauhan
  */
-@RefreshScope
+
 @RestController
 @RequestMapping("/document")
 public class DocumentController {
